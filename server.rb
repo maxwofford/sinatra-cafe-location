97daf739bd9aa42f532562557badcbfbd95f8c58
Max Wofford <max.speed.wofford@gmail.com>
2014-09-27 21:29:06 -0600

    search for local cafes

--- server.rb
+++ server.rb
@@ -6,7 +6,18 @@ require 'pry' if ENV['RACK_ENV'] == 'development'
 
 # Model
 class Cafe < ActiveRecord::Base
-	self.table_name = 'cafes'
+	extend Geocoder::Model::ActiveRecord
+	self.table_name = 'cafes' #'cafe'.pluralize defaults to 'caves'
+
+	geocoded_by :full_address 
+	after_validation :geocode
+
+	reverse_geocoded_by :latitude, :longitude
+	after_validation :reverse_geocode  
+
+	def full_address
+		"#{address}, #{zipcode}, #{city}, #{country}"
+	end
 end
 
 # Routes
@@ -15,11 +26,23 @@ get '/api/status' do
 end
 
 get '/api/cafe/:id' do
-	Cafe.find(params[:id]).to_json
+	if Cafe.find(params[:id])	
+		Cafe.find(params[:id]).to_json
+	else
+		"Cafe not found"
+	end
 end
 
-get '/api/cafe/location/:lat/:lng' do
-	Cafe.find(params[:id]).to_json
+get '/api/location/:lat/:lng/:distance' do
+	# Find cafe id by geocoding
+	@local_cafes = Cafe.near('#{params[:lat]},#{params[:lng]}', params[:distance])
+	@local_cafes.to_json
+end
+
+get '/api/location/:lat/:lng' do
+	# Find cafe id by geocoding
+	@local_cafes = Cafe.near('#{params[:lat]},#{params[:lng]}', 5)
+	@local_cafes.to_json
 end
 
 post '/api/cafe' do
